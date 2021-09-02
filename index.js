44aab510a35813609b3d500f887cc1f372ed7f45
Igor da Silva <igoratendimento@outlook.com.br>
2021-09-02 19:57:00 -0300

    Feitos comentarios para fácil entendimento

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 
 //Setando ejs como view engine
 app.set('view engine', 'ejs');
-//
+//Define Pasta public como padão para documentos static
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
   res.render('index');
 });
-
+//Retorna no console caso não haja erro
 app.listen(8080,()=>{
   console.log('App rodando!');
 })
