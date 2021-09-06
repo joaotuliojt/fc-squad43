ce89ed7a76a131078f12db4c31b38d4d8d3ada45
Igor da Silva <igoratendimento@outlook.com.br>
2021-09-06 20:23:44 -0300

    Adicionado a rota da tela initial

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const indexController = require("../controllers/indexController");
 const sedesController = require("../controllers/sedesController");
 const historicController = require("../controllers/historicController");
 const reserveController = require("../controllers/reserveController");
+const initialController = require("../controllers/initialController");
 
 //Rotas 
 router.get("/", indexController.load);
@@ -13,5 +14,7 @@ router.get("/sedes", sedesController.load);
 router.get("/historic", historicController.load);
 
 router.get("/reserve", reserveController.load);
+
+router.get("/initial", initialController.load);
 
 module.exports = router;
