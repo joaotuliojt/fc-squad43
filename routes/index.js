ed1856f3c9997ce1b8cf381a4820c992e67ed554
Igor da Silva <igoratendimento@outlook.com.br>
2021-09-06 19:53:29 -0300

    Adcionado rota para a tela reserve

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const router = require("express").Router();
 const indexController = require("../controllers/indexController");
 const sedesController = require("../controllers/sedesController");
 const historicController = require("../controllers/historicController");
+const reserveController = require("../controllers/reserveController");
 
+//Rotas 
 router.get("/", indexController.load);
 
 router.get("/sedes", sedesController.load);
 
 router.get("/historic", historicController.load);
+
+router.get("/reserve", reserveController.load);
 
 module.exports = router;
