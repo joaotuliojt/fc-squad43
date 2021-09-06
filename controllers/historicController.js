6a939d4913410203d56b066093b6dc2d64ec183c
Igor da Silva <igoratendimento@outlook.com.br>
2021-09-06 18:14:09 -0300

    Criado documento do controle da tela historico e adcionado a rota da tela

---
+++ controllers/historicController.js
@@ -0,0 +1,5 @@
+module.exports = {
+    load(req, res) {
+      res.render("historic");
+    },
+  };
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 //Controllers
 const indexController = require("../controllers/indexController");
 const sedesController = require("../controllers/sedesController");
+const historicController = require("../controllers/historicController");
 
 router.get("/", indexController.load);
 
 router.get("/sedes", sedesController.load);
+
+router.get("/historic", historicController.load);
 
 module.exports = router;
