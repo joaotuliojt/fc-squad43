261938e5b25b380d6a3ccae759c30e893f0fb196
Igor da Silva <igoratendimento@outlook.com.br>
2021-09-07 00:49:46 -0300

    Adicionado a biblioteca swiper para a relacao data em historic

---
+++ public/historic.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    mousewheel: true,
+    keyboard: true,
+})
