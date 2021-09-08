e9896cfa73f95bbde74306f6b357251be0c4639e
João Túlio <joaotuliosoares@hotmail.com>
2021-09-07 22:13:35 -0300

    Progress bar ajustada da tela de sedes

--- public/javascript/gauge.js
+++ public/javascript/gauge.js
@@ -1,5 +1,6 @@
 // Responsavel pela transformacao do velocimetro
 const gaugeElement = document.querySelector(".gauge");
+const gaugeElement2 = document.querySelector("#gauge2");
 
 function setGaugeValue(gauge, value) {
   if (value < 0 || value > 1) {
@@ -14,4 +15,18 @@ function setGaugeValue(gauge, value) {
   )}%`;
 }
 
+function setGaugeValue2(gauge, value) {
+  if (value < 0 || value > 1) {
+    return;
+  }
+
+  gauge.querySelector(".gauge__fill").style.transform = `rotate(${
+    value / 2
+  }turn)`;
+  gauge.querySelector(".gauge__cover").textContent = `${Math.round(
+    value * 100
+  )}%`;
+}
+
 setGaugeValue(gaugeElement, 0.823);
+setGaugeValue2(gaugeElement2, 0.5);
