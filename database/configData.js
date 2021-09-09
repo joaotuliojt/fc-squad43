30cf0813e837e1146502264b1cd6db2f7718c754
Igor da Silva <igoratendimento@outlook.com.br>
2021-09-09 12:01:18 -0300

    Adicionado pop up na tela reserve ao corfirma agendamento

--- database/configData.js
+++ database/configData.js
@@ -1,8 +1,8 @@
-const Sequelize = require("sequelize");
+//const Sequelize = require("sequelize");
 
-const connection = new Sequelize("fcallendar", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+//const connection = new Sequelize("fcallendar", "root", "", {
+ // host: "localhost",
+ // dialect: "mysql",
+//});
 
-module.exports = connection;
+//module.exports = connection;
